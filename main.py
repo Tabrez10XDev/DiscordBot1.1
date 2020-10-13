d7ee79cd03f649878d9a5796da06963f0f4c1ea0
Lj <meet.tabrezt1@gmail.com>
2020-10-13 13:03:33 +0530

    Implemented task template

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 import discord
 import CONST
+import time
+import asyncio
 token = CONST.token
 client = discord.Client()
 
+messages = joined = 0
 #https://discord.com/oauth2/authorize?client_id=764893385932406804&scope=bot&permissions=0
+#739833441649950751
+
+# async def update_stats():
+#     await client.wait_until_ready()
+#     global messages, joined
+#     while not client.is_closed():
+#         try:
+#             with open("stats.txt","a") as f:
+#                 f.write(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n")
+#             await asyncio.sleep(3600)
+#         except Exception as e:
+#             print(e)
+#             await asyncio.sleep(10)
+
+
+
+@client.event
+async def on_member_join(member):
+    global joined
+    joined+=1
+    vasakaal = await client.fetch_channel(739833442119712911)
+
+    await vasakaal.send(f"""A wild {member.mention} appeared""")
+
+
 
 @client.event
 async def on_message(message):
-    print(message.content)
-    print(message)
+    global messages, joined
+    messages+=1
+    id = client.get_guild(739833441649950751)
 
+
+    if message.content.find("!hello") != -1:
+        await message.channel.send("Hi")
+    elif message.content == "!users":
+        await message.channel.send(f"""{id.member_count} mates are in this channel""")
+    elif message.content == "!stats":
+        await message.channel.send(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}")
+
+#client.loop.create_task(update_stats())
 client.run(token)
